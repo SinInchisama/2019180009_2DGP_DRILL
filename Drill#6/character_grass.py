58e9d3c28da56472df781bf220da0234ec11da1a
SinInchisama <sun98390123@gmail.com>
2022-09-21 17:34:39 +0900

    drill06
    2019180009 김선빈입니다

--- Drill#6/character_grass.py
+++ Drill#6/character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -9,6 +10,12 @@ character = load_image('character.png')
 while True:
         x = 400
         y = 90
+        Angle = 0
+        while Angle< 361:
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(x + x * math.sin(Angle / 360 *2*math.pi),y + 200 - 200 * math.cos(Angle / 360 * 2 * math.pi))
+                Angle = Angle + 1
         while x<770:
                 clear_canvas_now()
                 grass.draw_now(400,30)
